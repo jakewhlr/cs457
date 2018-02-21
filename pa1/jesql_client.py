95ac2c8e042342f62908c791cb38b1567eb4dfcf
Jake Wheeler <jakewheeler@Jakes-MacBook-Pro.local>
2018-02-20 16:18:47 -0800

    added empty database creation, added CREATE options to settings.conf

--- pa1/jesql_client.py
+++ pa1/jesql_client.py
@@ -1,6 +1,8 @@
 """This module handles all of the user interaction functionality
 """
 import configparser
+import os
+import sys
 
 class Interface(object):
     """Class docstring"""
@@ -25,10 +27,24 @@ class Interface(object):
             except EOFError:
                 return self.__exit__ # This might need arguments xd
 
-            if not read_input.startswith(self.default_config['CommentPrefix']):
-                print('You said: ' + read_input.strip())
+            split_input = read_input.split(" ")
+
+            if split_input[0].startswith(self.default_config['CommentPrefix']):
+                print('Comment: ' + read_input)
+
             if read_input.strip() == self.default_config['ExitCommand']:
                 break
+
+            if split_input[0].strip() == self.default_config['CreateCommand']:
+                if not split_input[1] in self.create_commands: # if command doesn't exist
+                    print("INVALID OPTION: " + split_input[1], file=sys.stderr) # stderr
+
+                if len(split_input) < 3: # if no 3rd option
+                    print("\"CREATE " + self.create_commands['database'] + "\" Requires a name.", file=sys.stderr) # stderr
+
+                elif split_input[1] == self.create_commands['database']: # if option is database
+                    self.create_db(split_input[2])
+
 
     def read_config_file(self, filename):
         """Reads in a specified config file. Currently it will only
@@ -37,3 +53,20 @@ class Interface(object):
         config = configparser.ConfigParser()
         config.read(filename)
         self.default_config = config['DEFAULT']
+        self.create_commands = config['CREATE']
+
+    def create_db(self, name):
+        """Creates database as directory"""
+        current_dir = os.getcwd()
+        database_dir = os.path.join(current_dir, "databases")
+
+        if not os.path.exists(database_dir): # if databases dir doesn't exist
+            os.makedirs(database_dir) # create it
+
+        if os.path.exists(database_dir + "/" + name):
+            print("Database \"" + name + "\" exists.", file=sys.stderr) # stderr?
+        else:
+            os.makedirs(database_dir + "/" + name)
+
+    def create_table(self, name):
+        """Creates table as file"""
