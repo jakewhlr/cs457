b874ba6851a2fddd6763e5100ff7225d9ff9ded0
Jake Wheeler <jakewheeler@Jakes-MacBook-Pro.local>
2018-02-20 16:54:09 -0800

    Added commands config section, added invalid command error checking

--- pa1/jesql_client.py
+++ pa1/jesql_client.py
@@ -32,18 +32,24 @@ class Interface(object):
             if split_input[0].startswith(self.default_config['CommentPrefix']):
                 print('Comment: ' + read_input)
 
-            if read_input.strip() == self.default_config['ExitCommand']:
+            if read_input.strip() == self.commands_config['ExitCommand']:
                 break
 
-            if split_input[0].strip() == self.default_config['CreateCommand']:
-                if not split_input[1] in self.create_commands: # if command doesn't exist
-                    print("INVALID OPTION: " + split_input[1], file=sys.stderr) # stderr
+            if not split_input[0] in self.commands_config:
+                print("ERROR: " + split_input[0] + ": Command not found.", file=sys.stderr)
 
-                if len(split_input) < 3: # if no 3rd option
-                    print("\"CREATE " + self.create_commands['database'] + "\" Requires a name.", file=sys.stderr) # stderr
+            if split_input[0].strip() == self.commands_config['CreateCommand']:
+                if len(split_input) < 2:
+                    print("ERROR: " + split_input[0] + ": No option specified.", file=sys.stderr) # stderr
 
-                elif split_input[1] == self.create_commands['database']: # if option is database
-                    self.create_db(split_input[2])
+                elif not split_input[1] in self.create_options: # if command doesn't exist
+                    print("ERROR: " + split_input[0] + ": Invalid type: " + split_input[1], file=sys.stderr) # stderr
+
+                elif split_input[1] == self.create_options['database']: # if option is database
+                    if len(split_input) < 3: # if no 3rd option
+                        print("ERROR: CREATE " + self.create_options['database'] + ": Option requires a name.", file=sys.stderr) # stderr
+                    else:
+                        self.create_db(split_input[2])
 
 
     def read_config_file(self, filename):
@@ -53,7 +59,8 @@ class Interface(object):
         config = configparser.ConfigParser()
         config.read(filename)
         self.default_config = config['DEFAULT']
-        self.create_commands = config['CREATE']
+        self.commands_config = config['COMMANDS']
+        self.create_options = config['CREATE_OPTS']
 
     def create_db(self, name):
         """Creates database as directory"""
@@ -64,7 +71,7 @@ class Interface(object):
             os.makedirs(database_dir) # create it
 
         if os.path.exists(database_dir + "/" + name):
-            print("Database \"" + name + "\" exists.", file=sys.stderr) # stderr?
+            print("ERROR: Database \"" + name + "\" exists.", file=sys.stderr) # stderr?
         else:
             os.makedirs(database_dir + "/" + name)
 
