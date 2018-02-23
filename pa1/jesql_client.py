4cac60f49f0c556b4accb95a9f006bf94f95a11e
scheung3 <suifcheung@gmail.com>
2018-02-22 16:15:04 -0800

    added comment for deleting table and database; added Use function

--- pa1/jesql_client.py
+++ pa1/jesql_client.py
@@ -39,6 +39,7 @@ class Interface(object):
             if not split_input[0] in self.commands_config:
                 print("ERROR: " + split_input[0] + ": Command not found.", file=sys.stderr)
 
+            # CREATE
             if split_input[0].strip() == self.commands_config['CreateCommand']:
                 if len(split_input) < 2:
                     print("ERROR: " + split_input[0] + ": No option specified.", file=sys.stderr) # stderr
@@ -46,24 +47,40 @@ class Interface(object):
                 elif not split_input[1] in self.create_options: # if command doesn't exist
                     print("ERROR: " + split_input[0] + ": Invalid type: " + split_input[1], file=sys.stderr) # stderr
 
+                # Create database
                 elif split_input[1] == self.create_options['database']: # if option is database
                     if len(split_input) < 3: # if no 3rd option
                         print("ERROR: CREATE " + self.create_options['database'] + ": Option requires a name.", file=sys.stderr) # stderr
                     else:
                         self.create_db(split_input[2])
+
+                # create table
+                if split_input[0].strip() == self.commands_config['CreateCommand']:
+                    if  split_input[1] == self.create_options['table']:
+                #        self.create_table(split_input[2])
+                        print ("split_input[2]: ", split_input[2])
+                        print ("split_input[3]: ", split_input[3])
+                        print ("split_input[4]: ", split_input[4])
+                        print ("split_input[5]: ", split_input[5])
+                        print ("split_input[6]: ", split_input[6])
+
+
+            # DROP
             if split_input[0].strip() == self.commands_config['DeleteCommand']:
                 # delete database
-                if split_input[1] == self.create_options['database']: # if option is database
-                    self.delete_db((split_input[2]))
+                if split_input[1] == self.create_options['database']:
+                    self.delete_db(split_input[2])
 
-            if split_input[0].strip() == self.commands_config['DeleteCommand']:
-                # delete database
-                if split_input[1] == self.create_options['database']: # if option is database
-                    self.delete_db((split_input[2]))
+                # delete table
+                elif  split_input[1] == self.create_options['table']:
+                    self.delete_table(split_input[2])
+
+            # USE
+            if split_input[0].strip() == self.commands_config['UseCommand']:
+                self.use_db(split_input[1])
 
 
-
-    def read_config_file(self, filename):
+    def read_config_file(self, filename):python ignore parentheses
         """Reads in a specified config file. Currently it will only
         read in settings under the DEFAULT heading. Should prolly
         fix"""
@@ -72,7 +89,7 @@ class Interface(object):
         self.default_config = config['DEFAULT']
         self.commands_config = config['COMMANDS']
         self.create_options = config['CREATE_OPTS']
-        #self.delete_options =
+
 
     def create_db(self, name):
         """Creates database as directory"""
@@ -98,6 +115,7 @@ class Interface(object):
 
         # check if databse exist
         if os.path.exists(database_dir + "/" + name):
+            # delete the entire dir & files inside
             shutil.rmtree(database_dir + "/" + name)
         else:
             print ("!Failed to delete",name,"because it does not exist.")
@@ -109,12 +127,24 @@ class Interface(object):
 
         # check if table exist
         if os.path.exists(database_dir + "/" + name):
+            # remove file only
             os.remove(database_dir + "/" + name)
         else:
             print ("!Failed to delete",name,"because it does not exist.")
 
 
     # USE FOR db
+    def use_db(self, name):
+        """use named database"""
+        current_dir = os.getcwd()
+        database_dir = os.path.join(current_dir, "databases")
+
+        # check if databse exist
+        if os.path.isdir(database_dir + "/" + name):
+            os.chdir(database_dir + "/" + name)
+        else:
+            print ("!Failed to delete",name,"because it does not exist.")
+
 
     # SELECT for table
 
