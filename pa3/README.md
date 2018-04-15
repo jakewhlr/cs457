bda6ca526f07d105ed65ad5bb6ff6585490cb7d5
jakewhlr <jakewhlr@gmail.com>
2018-04-15 16:48:24 -0700

    updated argument parser for select, altered select's return type to return list of strings

---
+++ pa3/README.md
@@ -0,0 +1,111 @@
+# CS457 Database Management Systems
+## Programming Assignment 3: Table Joins
+#### Sui Cheung, Eric Olson, Jake Wheeler
+
+## Assignment Overview
+"In this assignment you will write a program that allows a database user to make queries over multiple tables. That is, you will implement table joins using techniques we discussed in the class. This assignment assumes the metadata and data manipulations on singular tables are already available, which should have been provided (implemented) in the  rst two programming assignments."
+
+## Implementation
+This database metadata management application is implemented using Python 3, and allows for the creation, deletion, and altering of databases and tables. Databases are created as subdirectories within the `databases` directory in the root of the repository. Within those directories, tables are created with the format:
+```
+attribute_name1 attribute_type1 | attribute_name2 attribute_type2
+value1 | value2
+```
+Attributes that are selected from tables are loaded into two dimensional lists and parsed to output any associated values, or alter attributes.
+
+## Usage
+The program can be run with: `python3 app.py`
+The following commands can be used:
+```
+CREATE DATABASE <database name>;
+CREATE TABLE <table name>(<attributes>);
+USE <database name>;
+INSERT into <table name> values(<values>);
+UPDATE <table name> SET <attribute> = <value> WHERE <attribute> = <value>;
+SELECT <attribute, *> FROM <table name>;
+DELETE from <table name> WHERE <attribute> = <value>;
+DROP DATABASE <database name>;
+DROP TABLE <table name>;
+ALTER <table name> ADD <attribute_name> <attribute_type>;
+.EXIT
+--<Comment>
+```
+
+### Sample Input
+```
+CREATE DATABASE CS457_PA2;
+USE CS457_PA2;
+CREATE TABLE Product (pid int, name varchar(20), price float);
+
+--Insert new data (20 points)
+insert into Product values(1,	'Gizmo',      	19.99);
+insert into Product values(2,	'PowerGizmo', 	29.99);
+insert into Product values(3,	'SingleTouch', 	149.99);
+insert into Product values(4,	'MultiTouch', 	199.99);
+insert into Product values(5,	'SuperGizmo', 	49.99);
+
+select * from Product;
+
+--Modify data (20 points)
+update Product
+set name = 'Gizmo'
+where name = 'SuperGizmo';
+
+update Product
+set price = 14.99
+where name = 'Gizmo';
+
+select * from Product;
+
+--Delete data (20 points)
+delete from product
+where name = 'Gizmo';
+
+delete from product
+where price > 150;
+
+select * from Product;
+
+--Query subsets (10 points)
+select name, price
+from product
+where pid != 2;
+
+.exit
+```
+
+### Expected output
+```
+-- Expected output
+--
+-- Database CS457_PA2 created.
+-- Using database CS457_PA2.
+-- Table Product created.
+-- 1 new record inserted.
+-- 1 new record inserted.
+-- 1 new record inserted.
+-- 1 new record inserted.
+-- 1 new record inserted.
+-- pid int|name varchar(20)|price float
+-- 1|Gizmo|19.99
+-- 2|PowerGizmo|29.99
+-- 3|SingleTouch|149.99
+-- 4|MultiTouch|199.99
+-- 5|SuperGizmo|49.99
+-- 1 record modified.
+-- 2 records modified.
+-- pid int|name varchar(20)|price float
+-- 1|Gizmo|14.99
+-- 2|PowerGizmo|29.99
+-- 3|SingleTouch|149.99
+-- 4|MultiTouch|199.99
+-- 5|Gizmo|14.99
+-- 2 records deleted.
+-- 1 record deleted.
+-- pid int|name varchar(20)|price float
+-- 2|PowerGizmo|29.99
+-- 3|SingleTouch|149.99
+-- name varchar(20)|price float
+-- SingleTouch|149.99
+-- All done.
+```
---
+++ pa3/jesql_parser.py
@@ -0,0 +1,158 @@
+import jesql_commands
+import sys
+import tokenizer
+
+def parse(tokens):
+    sub_parser = 'parse_' + tokens[0].lower()
+    if sub_parser not in dir(sys.modules[__name__]):
+        print('ERROR: near "' + tokens[0] + '": syntax error', file=sys.stderr)
+    else:
+        globals()[sub_parser](tokens[1:])
+
+def parse_create(tokens):
+    if tokens[0].lower() == 'database':
+        if len(tokens[1:]) == 1:
+            jesql_commands.create_db(tokens[1].lower())
+        else:
+            print('ERROR: near "' + tokens[-1] + '": syntax error', file=sys.stderr)
+
+    elif tokens[0].lower() == 'table':
+        if len(tokens[1:]) == 2:
+            value_list = tokenizer.decapsulate_values(tokens[2])
+            value_list = [value.strip() for value in value_list]
+            jesql_commands.create_table(tokens[1].lower(), value_list)
+        else:
+            print('ERROR: near "' + tokens[-1] + '": syntax error', file=sys.stderr)
+
+    else:
+        print('ERROR: near "' + tokens[0] + '": syntax error', file=sys.stderr)
+
+def parse_drop(tokens):
+    if tokens[0].lower() == 'database':
+        if len(tokens[1:]) == 1:
+            jesql_commands.drop_db(tokens[1].lower())
+        else:
+            print('ERROR: near "' + tokens[-1] + '": syntax error', file=sys.stderr)
+
+    elif tokens[0].lower() == 'table':
+        if len(tokens[1:]) == 1:
+            jesql_commands.drop_table(tokens[1].lower())
+        else:
+            print('ERROR: near "' + tokens[-1] + '": syntax error', file=sys.stderr)
+
+    else:
+        print('ERROR: near "' + tokens[0] + '": syntax error', file=sys.stderr)
+
+def parse_use(tokens):
+    if len(tokens) == 1:
+        jesql_commands.use(tokens[0].lower())
+    else:
+        print('ERROR: near "' + tokens[-1] + '": syntax error', file=sys.stderr)
+
+# TODO
+def parse_select(tokens):
+    # parse columns, place into sublists
+    list_arg = []
+    newargs = []
+    for index, arg in enumerate(tokens):
+        if arg.strip().lower() == 'from':
+            from_index = index
+            break
+        list_arg.append(arg)
+    for index in range(0, len(list_arg)):
+        del tokens[0]
+    for index, item in enumerate(list_arg):
+        list_arg[index] = item.replace(',', '')
+    tokens.insert(0, list_arg)
+
+    # parse tables list, place into sublists
+    list_item = []
+    list_arg = []
+    where_index = len(tokens)
+    for index, arg in enumerate(tokens):
+        if index < 2:
+            continue
+        if arg.strip().lower() == 'where':
+            where_index = index
+            break
+        list_item.append(arg)
+        if arg.endswith(','):
+            for list_index, item in enumerate(list_item):
+                list_item[list_index] = item.replace(',', '')
+            list_arg.append(list_item)
+            list_item = []
+    if list_item:
+        list_arg.append(list_item)
+    for index in range (1, where_index):
+        del tokens[1]
+    list_arg.insert(0, 'from')
+    tokens.insert(1, list_arg)
+
+    # parse where list, place into sublists
+    list_item = []
+    list_arg = []
+    for index, arg in enumerate(tokens):
+        if index < 3:
+            continue
+        list_item.append(arg)
+
+    for index in range (2, len(tokens)):
+        del tokens[2]
+    if list_item:
+        list_arg.append('where')
+        list_arg.append(list_item)
+        tokens.append(list_arg)
+
+    output = jesql_commands.select(tokens)
+    for line in output:
+        print(line)
+
+def parse_alter(tokens):
+    if tokens[0].lower() == 'table':
+        if len(tokens[1:]) == 4:
+            jesql_commands.alter(tokens[1].lower(), tokens[3], tokens[4])
+        else:
+            print('ERROR: near "' + tokens[-1] + '": syntax error', file=sys.stderr)
+
+    else:
+        print('ERROR: near "' + tokens[0] + '": syntax error', file=sys.stderr)
+
+def parse_insert(tokens):
+    if tokens[0].lower() == 'into':
+        if len(tokens[1:]) == 3:
+            value_list = tokenizer.decapsulate_values(tokens[3])
+            value_list = [value.strip() for value in value_list]
+
+            jesql_commands.insert(tokens[1].lower(), value_list)
+        else:
+            print('ERROR: near "' + tokens[-1] + '": syntax error', file=sys.stderr)
+
+    else:
+        print('ERROR: near "' + tokens[0] + '": syntax error', file=sys.stderr)
+
+def parse_delete(tokens):
+    try:
+        if tokens[0].lower() == 'from':
+            if tokens[2].lower() == 'where':
+                jesql_commands.delete(tokens[1].lower(), tokens[4], tokens[3], tokens[5])
+            else:
+                print('ERROR: near "' + tokens[2] + '": syntax error', file=sys.stderr)
+        else:
+            print('ERROR: near "' + tokens[0] + '": syntax error', file=sys.stderr)
+    except IndexError as e:
+        print('ERROR: expected arguments missing')
+
+def parse_update(tokens):
+    try:
+        if tokens[1].lower() == 'set':
+            if tokens[3] == '=' and tokens[7] == '=':
+                if tokens[5].lower() == 'where':
+                    jesql_commands.update(tokens[0].lower(), tokens[3], tokens[2], tokens[4], tokens[6], tokens[8])
+                else:
+                    print('ERROR: near "' + tokens[5] + '": syntax error', file=sys.stderr)
+            else:
+                print('ERROR: near "' + tokens[3] + '": syntax error', file=sys.stderr)
+        else:
+            print('ERROR: near "' + tokens[1] + '": syntax error', file=sys.stderr)
+    except IndexError as e:
+        print('ERROR: expected arguments missing')
